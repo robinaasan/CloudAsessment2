d3b2e4ad1fcdc674a58ac5b900f0f8d54ef6d4c5
Robin Aasan <robin.aasan@hotmail.com>
2022-10-26 16:19:46 +1000

    added tensor module

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); //TODO: remove after build
 app.use('/api/testRoute', testRoute);
+app.use('/api/tensor', testRoute);
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening at http://localhost:${process.env.PORT}`)
